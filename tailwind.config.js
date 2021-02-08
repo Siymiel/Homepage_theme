b65fce1a02423713111b9f8e301a717af64a9aa4
Samuel Kinuthia <skinuthia77@gmail.com>
2021-02-08 20:33:53 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+  './*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+	require('@tailwindcss/ui'),
+  ],
+}
